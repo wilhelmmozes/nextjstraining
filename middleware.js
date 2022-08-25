ca5757f2791d754f00575dc76acce95eac5646ba
Wilhelm Mozes <wilhelm.mozes@sirclo.com>
2022-08-25 15:44:28 +0700

    add middleware

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ import {NextResponse} from 'next/server';
 
 export default function middleware(request){
     const status = request.cookies.get('status');
+    const role = request.cookies.get('role');
     // console.log("ini request", request);
     const url = request.url;
     const {origin} = request.nextUrl;
@@ -9,8 +10,9 @@ export default function middleware(request){
 
     if(!status){
         console.log("status kosong");
-        // return NextResponse.rewrite(`${origin}/backoffice/login`);
+        return NextResponse.rewrite(`${origin}/backoffice/login`);
     }
+    if(role)
     console.log("status ada");
     // return NextResponse.rewrite(`${request.nextUrl}`);
 
--- pages/backoffice/login.js
+++ pages/backoffice/login.js
@@ -3,8 +3,11 @@ import React from 'react';
 export default function Login(){
     return (
         <div>
-            <button onClick={() => {document.cookie=`status=authenticated;path:/backoffice`}}>
+            <button onClick={() => {document.cookie=`status=authenticated;path:/backoffice;role="admin"`}}>
                 Login as user
+            </button>
+            <button onClick={() => {document.cookie=`status=authenticated;path:/backoffice;role="super"`}}>
+                Login as superadmin
             </button>
         </div>
     )
